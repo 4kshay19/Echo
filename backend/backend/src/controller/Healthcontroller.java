cc4d91715a78d3923bb81c5becf87869ba1add42
Akshay <akshaykasi19@gmail.com>
2026-08-12 22:38:41 +0530

    Initialize Echo

---
+++ backend/backend/src/controller/Healthcontroller.java
@@ -0,0 +1,13 @@
+package com.echo.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Echo backend is running!";
+    }
+}
